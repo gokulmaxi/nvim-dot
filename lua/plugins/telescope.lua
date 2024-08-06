52c69ac48fa8b2f31bb554a7987bde607dd71244
gokulmaxi <gokulakams@gmail.com>
2024-08-06 23:19:25 +0530

    [ADD] added which key config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,6 +7,7 @@ return {
       tag = "0.1.5",
       dependencies = { "nvim-lua/plenary.nvim" },
       config = function()
+	local wk = require("which-key")
         require("telescope").setup({
           extensions = {
             ["ui-select"] = {
@@ -15,6 +16,12 @@ return {
           },
         })
         local builtin = require("telescope.builtin")
+	wk.add({
+		  { "<leader>f", group = "file" }, -- group
+		  { "<leader>ff", builtin.find_files , desc = "Find Files" }, -- group
+		  { "<leader>fg", builtin.live_grep, desc = "Live Grep" }, -- group
+		  { "<leader><leader>", builtin.oldfiles, desc = "Recent Files" }, -- group
+	})
         require("telescope").load_extension("ui-select")
       end,
     },
